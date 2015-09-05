94fb57b858cce938fd2465bdd6e158d9c00c2242
Alan Smith <alan@randomsmiths.com>
2015-09-05 14:19:24 +0000

    Added support for Phant

--- rfid_test.py
+++ rfid_test.py
@@ -1,16 +1,36 @@
 from keyboard_alike import reader
-
-reader = reader.Reader(0x08ff, 0x0009, 84, 16, should_reset=False) # From the documentation - the VID and DEVID
-
-reader.initialize()
+import datetime
+from phant import Phant
 
 tags = {'0009909662' : 'Philip Smith',
         '0004406858' : 'Joshua Smith',
         '0009909876' : 'Abigail Smith',
         '0004304910' : 'Dad'}
+p = Phant('yAYZ9aJ98Kiyz4XNm5NW', 'location', 'id', 'name', 'time', private_key='4Wqom46m9niK2k8pzxp4')
+
+def getName(idString):
+   try:
+     return tags[card]
+   except:
+     return 'Unknown: ' + idString
+
+def logAccess(id):
+  name = getName(id)
+  time = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+  p.log("Hopewell", id, name, time)
+  f = open('/home/pi/rfid/access_log.txt','a')
+  f.write("{idStr},{nameStr},{datetime}\n".format(idStr=id,nameStr=name,datetime=time))
+  f.close()
+
+reader = reader.Reader(0x08ff, 0x0009, 84, 16, should_reset=False) # From the documentation - the VID and DEVID
+
+reader.initialize()
 
 while(1):
    card = reader.read().strip()    # get the card number
-   print "Hi %s! RFID card: %s" % (tags[card],card)
+   f = open('/home/pi/rfid/web/message.txt', 'w')
+   f.write("Hi {name}".format(name=getName(card))) 
+   f.close()
+   logAccess(card)
 
 reader.disconnect()
