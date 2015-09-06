4f9a685a2cb57316249a129503a48706b1d84208
Alan Smith <alan@randomsmiths.com>
2015-09-06 02:17:14 +0000

    Added ability to display who all is in the building

--- rfid_test.py
+++ rfid_test.py
@@ -5,10 +5,10 @@ from phant import Phant
 
 p = Phant('yAYZ9aJ98Kiyz4XNm5NW', 'location', 'id', 'name', 'time', private_key='4Wqom46m9niK2k8pzxp4')
 
-def getName(idString):
+def getName(idString, students):
 	try:
-		person[idString]['isHere'] = person[idString]['isHere'] == False
-		return person[idString]['name']
+		students[idString]['isHere'] = students[idString]['isHere'] == False
+		return students[idString]['name']
 	except:
 		return 'Unknown'
 
@@ -19,25 +19,47 @@ def logAccess(id, name):
   f.write("{idStr},{nameStr},{datetime}\n".format(idStr=id,nameStr=name,datetime=time))
   f.close()
 
-person = {}
 
-with open('/home/pi/rfid/web/users.csv', 'r') as csvfile:
+def readFile(fileName):
+   person = {}
+   with open(fileName, 'r') as csvfile:
 	csvreader = csv.reader(csvfile, delimiter=',')
 	for row in csvreader:
 		person[row[0]] = {'rfid': row[0], 'name': row[1], 'isHere': row[2] == 'True'}
+   return person;
+
+def writePresent(students):
+   any = False;
+   f = open('/home/pi/rfid/web/users.txt', 'w');
+   for id in students:
+      if(students[id]['isHere']):
+         f.write(students[id]['name'] +"<br/>");
+         any = True;
+   if(any == False):
+      f.write("None");
+   f.close()
+
+def writeMessage(id, students):
+   f = open('/home/pi/rfid/web/message.txt','w'); 
+   if id in students:
+      if(students[id]['isHere']):
+        f.write("Hi, ");
+      else:
+        f.write("Bye, ");
+      f.write(students[id]['name']);
+   f.close()
 
 reader = reader.Reader(0x08ff, 0x0009, 84, 16, should_reset=False) # From the documentation - the VID and DEVID
 
 reader.initialize()
-
+students = readFile("/home/pi/rfid/web/users.csv");
 while(1):
    card = reader.read().strip()    # get the card number
-   name = getName(card);
+   name = getName(card, students);
    print('\n' + name);
-   print(person);
-   f = open('/home/pi/rfid/web/message.txt', 'w')
-   f.write("Hi {name}".format(name=name))
-   f.close()
+   print(students);
+   writeMessage(card, students); 
    logAccess(card, name)
+   writePresent(students);
 
 reader.disconnect()
