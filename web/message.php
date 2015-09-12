4347d7690e51e4e7c91ed28afd7190debc6c4d0e
Alan Smith <alan@randomsmiths.com>
2015-09-12 19:49:29 +0000

    :q!

--- web/message.php
+++
@@ -1,6 +0,0 @@
-<?php
-      $myfile = fopen("message.txt", "r") or die("Unable to open file!");
-      $read=fread($myfile,filesize("message.txt"));
-      fclose($myfile);
-      echo $read;
-  ?>
--- web/users.php
+++
@@ -1,6 +0,0 @@
-<?php
-      $myfile = fopen("users.txt", "r") or die("Unable to open file!");
-      $read=fread($myfile,filesize("users.txt"));
-      fclose($myfile);
-      echo $read;
-  ?>
