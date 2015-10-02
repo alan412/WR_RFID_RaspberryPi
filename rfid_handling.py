72cd1f5be2369324c886d3bb2608e2742b543593
Alan Smith <alan@randomsmiths.com>
2015-10-01 22:27:06 -0400

    Changes to have header in csv file

--- rfid_handling.py
+++ rfid_handling.py
@@ -12,8 +12,10 @@ class members(object):
         self.students = {}
         with open(filename, 'r') as csvfile:
      	    csvreader = csv.reader(csvfile, delimiter=',')
+            headers = next(csvreader, None) # returns the headers or None if it is empty
      	    for row in csvreader:
-     		    self.students[row[0]] = {'rfid': row[0], 'name': row[1], 'isHere': False}
+                if row[0]:
+     		       self.students[row[0]] = {'rfid': row[0], 'name': row[1] + ' ' + row[2], 'isHere': False}
 
     def getGreeting(self, idString):
         if idString in self.students:
@@ -49,7 +51,7 @@ class members(object):
         f.close()
         
 class RFIDHandling(object):
-    def __init__(self, fileBase = "/home/pi/rfid/web/", location = "Hopewell"):
+    def __init__(self, fileBase = "/home/pi/rfid/", location = "Hopewell"):
         self.reader = reader.Reader(0x08ff, 0x0009, 84, 16, should_reset=False) # From the documentation - the VID and DEVID
         self.reader.initialize();
         self.location = location;
@@ -67,7 +69,7 @@ class RFIDHandling(object):
         card = self.reader.read().strip()    # get the card number
         self.members.toggleHere(card);
         self.logAccess(card, self.members.getName(card));
-        self.members.writePresent(self.fileBase + "users.txt")
+        self.members.writePresent(self.fileBase + "/web/users.txt")
         return self.members.getGreeting(card);
     def close(self):
         self.reader.disconnect()
